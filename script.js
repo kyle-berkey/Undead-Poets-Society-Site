f72fe9b92811efed8c42af19c388d56bcf52bb4d
JosephStalin <Berkeyboy1@gmail.com>
2022-02-03 14:29:13 -0500

    Fix for links on music page

--- script.js
+++ script.js
@@ -25,4 +25,64 @@ function popUpGen(title, desc) {
 	body.appendChild(popUp);
   }
   
-  // music nlides
+  // music slides
+
+  // starts with newest, goes to oldest
+	let slides = document.getElementsByClassName("slide");
+	let songs = document.getElementsByClassName('song');
+	let albums = document.getElementsByClassName('songs');
+
+  class Slide {
+	constructor() {
+		this.songList = [];
+		this.albumSongs = albums[slideIndex-1].children;;
+		for (let i = 0; i < this.albumSongs.length; i++) {	
+			this.songList.push(songs[i]);
+		}
+	}
+}
+
+  let slideIndex = 1;
+  let current = new Slide;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+	showSlides(slideIndex += n);
+	return current = new Slide;
+  }
+  
+  function currentSlide(n) {
+	showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+	let i;
+	let slides = document.getElementsByClassName("slide");
+	if (n > slides.length) {slideIndex = 1}    
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
+	}
+	slides[slideIndex-1].style.display = "block"; 
+  }
+
+let modal = document.getElementById('modal');
+function play(url) {
+	let iframe = document.getElementById('iframe');
+	iframe.src = url;
+	modal.style.display = 'block';
+}
+
+let clse = document.getElementById('close');
+clse.onclick = function closeModal() {
+modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
